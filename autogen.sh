4023bfb5e72318ca87edaf6ee61932dd727e9ceb
Greg A. Woods <woods@planix.com>
2018-03-22 21:07:19 -0700

    minimal changes to make this buildable again
    - especially in a VPATH build
    - and fix some compiler warnings
    
    Libtool still sucks -- this will be the end of Autotools support.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+# Run this to generate all the initial configure script
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
@@ -14,24 +14,9 @@ cd $srcdir
 
 PKG_NAME=`autoconf --trace 'AC_INIT:$1' configure.ac`
 
-if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
-	echo "*** WARNING: I am going to run \`configure' with no arguments." >&2
-	echo "*** If you wish to pass any to it, please specify them on the" >&2
-	echo "*** \`$0\' command line." >&2
-	echo "" >&2
-fi
-
 aclocal --install || exit 1
 autoreconf --verbose --force --install -Wno-portability || exit 1
+rm -rf autom4te.cache
 
-cd $olddir
-if [ "$NOCONFIGURE" = "" ]; then
-	$srcdir/configure "$@" || exit 1
-
-	if [ "$1" = "--help" ]; then exit 0 else
-		echo "Now type \`make\' to compile $PKG_NAME" || exit 1
-	fi
-else
-	echo "Skipping configure process."
-fi
+echo "Now make a build directory and run ../configure from there...."
 
